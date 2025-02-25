7415e409c40c0c4a4743efeff29b4ac872780775
onthewaveagain <62063840+onthewaveagain@users.noreply.github.com>
2025-02-25 15:30:40 +0000

    LLAPI new release

--- N64.sv
+++ N64.sv
@@ -870,6 +870,7 @@ always_comb begin
 				joystick_analog_l1 = joystick_usb_analog_l0;	
 				joystick_analog_l2 = joystick_usb_analog_l1;
 				joystick_analog_l3 = joystick_usb_analog_l2;
+				
 		end else if (use_llapi & ~use_llapi2) begin
                	joy = joy_ll_a;
                 joy2 = joy_usb_0;
@@ -880,17 +881,8 @@ always_comb begin
 				joystick_analog_l1 = joystick_usb_analog_l0;	
 				joystick_analog_l2 = joystick_usb_analog_l1;
 				joystick_analog_l3 = joystick_usb_analog_l2;
-		end else if (OSD_STATUS) begin
-               	joy = 0;
-                joy2 = 0;
-                joy3 = 0;
-                joy4 = 0;
-				
-				joystick_analog_l0 = {8'b0,8'b0};
-				joystick_analog_l1 = {8'b0,8'b0};	
-				joystick_analog_l2 = {8'b0,8'b0};
-				joystick_analog_l3 = {8'b0,8'b0};
-        end else begin
+		
+        end else if (use_llapi & use_llapi2) begin
                 joy = joy_ll_a;
                 joy2 = joy_ll_b;
                 joy3 = joy_usb_0;
@@ -900,6 +892,17 @@ always_comb begin
 				joystick_analog_l1 = {axis_ll_b_ly, axis_ll_b_lx};	
 				joystick_analog_l2 = joystick_usb_analog_l0;
 				joystick_analog_l3 = joystick_usb_analog_l1;
+				
+		end else begin
+                joy = 0;
+                joy2 = 0;
+                joy3 = 0;
+                joy4 = 0;
+				
+				joystick_analog_l0 = {8'b0,8'b0};
+				joystick_analog_l1 = {8'b0,8'b0};	
+				joystick_analog_l2 = {8'b0,8'b0};
+				joystick_analog_l3 = {8'b0,8'b0};
 		end
 end
 
